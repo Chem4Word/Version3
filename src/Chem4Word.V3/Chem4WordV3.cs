79bb3a63dbadad982c27cf68874ec7a9fe28270d
Mike Williams <mike@doublewide.co.uk>
2017-12-06 23:16:34 +0000

    Typing in comments seems to work now

--- src/Chem4Word.V3/Chem4WordV3.cs
+++ src/Chem4Word.V3/Chem4WordV3.cs
@@ -780,35 +780,43 @@ namespace Chem4Word
             {
                 Word.Document doc = sel.Application.ActiveDocument;
 
-                Word.WdContentControlType? contentControlType = null;
-
-                foreach (Word.ContentControl cc in doc.ContentControls)
+                if (sel.StoryType == Word.WdStoryType.wdMainTextStory)
                 {
-                    if (cc.Range.Start <= sel.Range.Start && cc.Range.End >= sel.Range.End)
+                    Word.WdContentControlType? contentControlType = null;
+
+                    foreach (Word.ContentControl cc in doc.ContentControls)
                     {
-                        contentControlType = cc.Type;
-                        if (cc.Title != null && cc.Title.Equals(Constants.ContentControlTitle))
+                        if (cc.Range.Start <= sel.Range.Start && cc.Range.End >= sel.Range.End)
                         {
-                            Debug.WriteLine($"  Selecting CC at {cc.Range.Start - 1} to {cc.Range.End + 1}");
-                            doc.Application.Selection.SetRange(cc.Range.Start - 1, cc.Range.End + 1);
+                            contentControlType = cc.Type;
+                            if (cc.Title != null && cc.Title.Equals(Constants.ContentControlTitle))
+                            {
+                                Debug.WriteLine($"  Selecting CC at {cc.Range.Start - 1} to {cc.Range.End + 1}");
+                                doc.Application.Selection.SetRange(cc.Range.Start - 1, cc.Range.End + 1);
+                            }
                         }
                     }
-                }
 
-                if (contentControlType == null || contentControlType == Word.WdContentControlType.wdContentControlRichText)
-                {
-                    if (doc.CompatibilityMode >= (int)Word.WdCompatibilityMode.wdWord2010)
+                    if (contentControlType == null || contentControlType == Word.WdContentControlType.wdContentControlRichText)
                     {
-                        ChemistrySelected = sel.ContentControls.Count > 0;
-
-                        if (ChemistrySelected)
+                        if (doc.CompatibilityMode >= (int)Word.WdCompatibilityMode.wdWord2010)
                         {
-                            Ribbon.ActivateChemistryTab();
-                            SetButtonStates(ButtonState.CanEdit);
+                            ChemistrySelected = sel.ContentControls.Count > 0;
+
+                            if (ChemistrySelected)
+                            {
+                                Ribbon.ActivateChemistryTab();
+                                SetButtonStates(ButtonState.CanEdit);
+                            }
+                            else
+                            {
+                                SetButtonStates(ButtonState.CanInsert);
+                            }
                         }
                         else
                         {
-                            SetButtonStates(ButtonState.CanInsert);
+                            ChemistrySelected = false;
+                            SetButtonStates(ButtonState.NoDocument);
                         }
                     }
                     else
