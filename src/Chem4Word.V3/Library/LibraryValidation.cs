d14c760e6d25f22f4ea61ce8eaf867ff9353afbb
Mike Williams <mike@doublewide.co.uk>
2017-12-06 15:04:55 +0000

    Add Headers

--- src/Chem4Word.V3/Library/LibraryValidation.cs
+++ src/Chem4Word.V3/Library/LibraryValidation.cs
@@ -1,3 +1,10 @@
-﻿namespace Chem4Word.Library
+﻿// ---------------------------------------------------------------------------
+//  Copyright (c) 2018, The .NET Foundation.
+//  This software is released under the Apache License, Version 2.0.
+//  The license and further copyright text can be found in the file LICENSE.md
+//  at the root directory of the distribution.
+// ---------------------------------------------------------------------------
+
+namespace Chem4Word.Library
 {
 }
--- src/Chemistry/Chem4Word.Model/Enums/Enums.cs
+++ src/Chemistry/Chem4Word.Model/Enums/Enums.cs
@@ -1,4 +1,11 @@
-﻿namespace Chem4Word.Model.Enums
+﻿// ---------------------------------------------------------------------------
+//  Copyright (c) 2018, The .NET Foundation.
+//  This software is released under the Apache License, Version 2.0.
+//  The license and further copyright text can be found in the file LICENSE.md
+//  at the root directory of the distribution.
+// ---------------------------------------------------------------------------
+
+namespace Chem4Word.Model.Enums
 {
     /// <summary>
     /// Specifies how the double bond is drawn with regards to the connecting
