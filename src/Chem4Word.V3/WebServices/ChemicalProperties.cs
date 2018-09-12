0f7714631c21f3236439d5e97afbd0d9b04bf102
Mike Williams <mike@doublewide.co.uk>
2018-09-12 15:43:05 +0100

    Add headers

--- src/Chem4Word.V3/WebServices/ChemicalProperties.cs
+++ src/Chem4Word.V3/WebServices/ChemicalProperties.cs
@@ -1,4 +1,11 @@
-﻿namespace Chem4Word.WebServices
+﻿// ---------------------------------------------------------------------------
+//  Copyright (c) 2018, The .NET Foundation.
+//  This software is released under the Apache License, Version 2.0.
+//  The license and further copyright text can be found in the file LICENSE.md
+//  at the root directory of the distribution.
+// ---------------------------------------------------------------------------
+
+namespace Chem4Word.WebServices
 {
     public class ChemicalProperties
     {
--- src/Chem4Word.V3/WebServices/ChemicalServices.cs
+++ src/Chem4Word.V3/WebServices/ChemicalServices.cs
@@ -1,4 +1,11 @@
-﻿using IChem4Word.Contracts;
+﻿// ---------------------------------------------------------------------------
+//  Copyright (c) 2018, The .NET Foundation.
+//  This software is released under the Apache License, Version 2.0.
+//  The license and further copyright text can be found in the file LICENSE.md
+//  at the root directory of the distribution.
+// ---------------------------------------------------------------------------
+
+using IChem4Word.Contracts;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
