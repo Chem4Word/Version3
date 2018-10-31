ccf0796ec1cdd6a0cae4f02cd04c63e5b401212a
Mike Williams <mike@doublewide.co.uk>
2018-10-31 22:40:18 +0000

    3.0.22 Release 9

--- src/Chemistry/Chem4Word.Model/Properties/AssemblyInfo.cs
+++ src/Chemistry/Chem4Word.Model/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.22.6892")]
-[assembly: AssemblyFileVersion("3.0.22.6892")]
+[assembly: AssemblyVersion("3.0.22.6878")]
+[assembly: AssemblyFileVersion("3.0.22.6878")]
