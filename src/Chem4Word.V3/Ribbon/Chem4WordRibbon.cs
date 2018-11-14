7e8820004b4209b98da9a5fbee40c51e1f84cc47
Mike Williams <mike@doublewide.co.uk>
2018-11-14 14:34:16 +0000

    Don't log ActivateChemistryTab() exception

--- src/Chem4Word.V3/Ribbon/Chem4WordRibbon.cs
+++ src/Chem4Word.V3/Ribbon/Chem4WordRibbon.cs
@@ -542,9 +542,13 @@ namespace Chem4Word
             {
                 this.RibbonUI.ActivateTab(Chem4WordV3.ControlId.ToString());
             }
-            catch (Exception ex)
+            //catch (Exception ex)
+            //{
+            //    new ReportError(Globals.Chem4WordV3.Telemetry, Globals.Chem4WordV3.WordTopLeft, module, ex).ShowDialog();
+            //}
+            catch
             {
-                new ReportError(Globals.Chem4WordV3.Telemetry, Globals.Chem4WordV3.WordTopLeft, module, ex).ShowDialog();
+                // Do Nothing
             }
         }
 
